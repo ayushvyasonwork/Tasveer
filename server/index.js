1a0e3a0502f6b22c5276cb8e5e540bd95c7f6114
Ayush Vyas <work.ayushvyas17@gmail.com>
2025-07-20 01:05:14 +0530

    fix: update CORS configuration to restrict origin and allow specific headers

--- server/index.js
+++ server/index.js
@@ -60,8 +60,9 @@ const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     // origin: "http://localhost:3000", // frontend URL
-    origin:"*",
+    origin:"tasveer-delta.vercel.app",
     methods: ["GET", "POST"],
+    allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   },
 });

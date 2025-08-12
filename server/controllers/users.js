c75d49c28918407c07e6b3ae64300938dd742733
ayushvyasonwork <work.ayushvyas17@gmail.com>
2025-08-13 00:24:56 +0530

    fix: update Navbar component to ensure consistent text color and improve friend retrieval logic in user controller feat: add new image assets for application

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -15,59 +15,85 @@ export const getUserFriends = async (req, res) => {
   try {
     const { id } = req.params;
     console.log('id in getUserFriends is ', id);
+
     const user = await User.findById(id);
     if (!user) {
       return res.status(404).json({ message: "User not found" });
     }
+
     const friends = await Promise.all(
-      user.friends.map((id) => User.findById(id))
+      user.friends.map((friendId) => User.findById(friendId))
     );
-    if (!friends) {
-      return res.status(404).json({ message: "Friends not found" });
-    }
-    const formattedFriends = friends.map(
+
+    // Filter out any null entries (if a friend was deleted or doesn't exist)
+    const validFriends = friends.filter(friend => friend !== null);
+
+    const formattedFriends = validFriends.map(
       ({ _id, firstName, lastName, occupation, location, picturePath }) => {
         return { _id, firstName, lastName, occupation, location, picturePath };
       }
     );
+
     console.log('formattedFriends in getUserFriends is ', formattedFriends);
     res.status(200).json(formattedFriends);
   } catch (err) {
-    res.status(404).json({ message: err.message });
+    console.error('Error in getUserFriends:', err);
+    res.status(500).json({ message: err.message });
   }
 };
+
 
 /* UPDATE */
 export const addRemoveFriend = async (req, res) => {
   try {
     const { id, friendId } = req.params;
+
     const user = await User.findById(id);
     const friend = await User.findById(friendId);
 
+    if (!user || !friend) {
+      return res.status(404).json({ message: "User or Friend not found" });
+    }
+
     if (user.friends.includes(friendId)) {
-      user.friends = user.friends.filter((id) => id !== friendId);
-      friend.friends = friend.friends.filter((id) => id !== id);
+      // Remove each other
+      user.friends = user.friends.filter((fid) => fid.toString() !== friendId);
+      friend.friends = friend.friends.filter((fid) => fid.toString() !== id);
     } else {
+      // Add each other
       user.friends.push(friendId);
       friend.friends.push(id);
     }
+
     await user.save();
     await friend.save();
 
+    // Get updated list of user's friends
     const friends = await Promise.all(
-      user.friends.map((id) => User.findById(id))
+      user.friends.map((fid) => User.findById(fid))
     );
-    const formattedFriends = friends.map(
-      ({ _id, firstName, lastName, occupation, location, picturePath }) => {
-        return { _id, firstName, lastName, occupation, location, picturePath };
-      }
+
+    // Filter out any nulls to avoid destructure error
+    const validFriends = friends.filter(friend => friend !== null);
+
+    const formattedFriends = validFriends.map(
+      ({ _id, firstName, lastName, occupation, location, picturePath }) => ({
+        _id,
+        firstName,
+        lastName,
+        occupation,
+        location,
+        picturePath
+      })
     );
 
     res.status(200).json(formattedFriends);
   } catch (err) {
-    res.status(404).json({ message: err.message });
+    console.error('Error in addRemoveFriend:', err);
+    res.status(500).json({ message: err.message });
   }
 };
+
 export const updateSocialLinks = async (req, res) => {
   try {
     const { id } = req.params;
